3206d363a1714d4a8ffb4a7cf947014ef8fbd7dc
Sh3lby007 <zhengshaobin@ymail.com>
2024-11-01 14:50:44 +0800

    comment out i18n

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 // Load bulma & font awesome styles in the main entry point before all else
 import "bulma/css/bulma.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css"
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-import i18n from './plugins/i18n'
+// import i18n from './plugins/i18n'
 
 import App from "./App.vue";
 import router from "./router";
@@ -13,5 +13,5 @@ import router from "./router";
 createApp(App)
   .use(createPinia().use(piniaPluginPersistedstate))
   .use(router)
-  .use(i18n)
+  // .use(i18n)
   .mount("#app");
